bd3ebad6d410cb7b73a30c70f3a3bff9612e3f1c
liruzun <125100032+liruzun@users.noreply.github.com>
2023-02-12 19:07:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const form = document.getElementById("form");
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const confirm_password= document.getElementById("confirm_password");
+const confirm_password = document.getElementById("confirm_password");
 
 
-function setError(element, errorMessage){
+function setError(element, errorMessage) {
   const parent = element.parentElement;
-  if(parent.classList.contains("success")){
+  if (parent.classList.contains("success")) {
     parent.classList.remove("success"); //saca success si ya está para que no se muestren ambas
   }
   parent.classList.add("error"); //defino constantes para reemplazar los parrafos predefinidos de acuerdo al error
-  
-  const id=element.id;
 
-  const paragraph= document.getElementById (`${id}-message`);//concatenacion id y message
+  const id = element.id;
+
+  const paragraph = document.getElementById(`${id}-message`);//concatenacion id y message
   paragraph.textContent = errorMessage;
 
 }
 
-function setSuccess(element){
+function setSuccess(element) {
   const parent = element.parentElement;
-  if(parent.classList.contains("error")){
+  if (parent.classList.contains("error")) {
     parent.classList.remove("error")//saca error si ya está para que no se muestren ambas
   }
   parent.classList.add("success");
@@ -29,54 +29,56 @@ function setSuccess(element){
 
 
 //funcion validacion de email (ya que no es required en HTML por default para poder correr el script)
-function isEmailValid(email){
-  const reg =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-return reg.test(email);
+function isEmailValid(email) {
+  const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return reg.test(email);
 }
 
 
-function validateForm(){
-//username
-  if(username.value.trim() ==""){
+function validateForm() {
+  //username
+  if (username.value.trim() == "") {
     setError(username, "Rellene este campo");
   }
-  else {setSuccess(username);
+  else {
+    setSuccess(username);
   }
-//email
-if(email.value.trim() ==""){
-  setError(email, "Rellene este campo");
-}
-else if(!isEmailValid(email.value)){
-setError(email,"Email Inválido");
-} else{
-  setSuccess(email);
-}
+  
+  //email
+  if (email.value.trim() == "") {
+    setError(email, "Rellene este campo");
+  }
+  else if (!isEmailValid(email.value)) {
+    setError(email, "Email Inválido");
+  } else {
+    setSuccess(email);
+  }
 
-//clave
-if(password.value.trim() ==""){
-  setError(password, "Rellene este campo");
-}
-else if(password.value.trim().length < 8){
-  setError(password,"Debe tener más de 8 caracteres");
-} else{
-  setSuccess(password);
-}
+  //clave
+  if (password.value.trim() == "") {
+    setError(password, "Rellene este campo");
+  }
+  else if (password.value.trim().length < 8) {
+    setError(password, "Debe tener más de 8 caracteres");
+  } else {
+    setSuccess(password);
+  }
 
-//confirmacion clave
-if(confirm_password.value.trim() ==""){
-  setError(confirm_password, "Rellene este campo");
-}
-else if(confirm_password !== password){
-  setError(confirm_password,"Las contraseñas no coinciden");
-}
-else if(confirm_password == password){
-  setSuccess(confirm_password);
-}
+  //confirmacion clave
+  if (confirm_password.value.trim() == "") {
+    setError(confirm_password, "Rellene este campo");
+  }
+  else if (confirm_password !== password) {
+    setError(confirm_password, "Las contraseñas no coinciden");
+  }
+  else if (confirm_password == password) {
+    setSuccess(confirm_password);
+  }
 
 }
 
 //Es mejor definir las funciones antes de usarlas
-form.addEventListener('submit',(event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   validateForm();
